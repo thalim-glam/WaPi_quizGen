e893558fbb8c60b20491fff3b9e284e2d266cd1d
Tasneem <thalim.glam@gmail.com>
2023-10-09 12:06:59 -0700

    Started to do the homework 4 with the tutor

---
+++ questions.js
@@ -0,0 +1,17 @@
+var questions = [
+  {
+    title: "How many sides are there in a triangle",
+    choices: ["3", "2", "0"],
+    answer: "3"
+  },
+  {
+    title: "How many sides are there in a circle",
+    choices: ["3", "2", "0"],
+    answer: "0"
+  },
+  {
+    title: "How many sides are there in a square",
+    choices: ["3", "2", "4"],
+    answer: "4"
+  }
+]
