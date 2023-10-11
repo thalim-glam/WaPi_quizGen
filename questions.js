ecf64b85262f5fd9df9e92b9eeba24292e7c39db
Tasneem <thalim.glam@gmail.com>
2023-10-11 16:38:52 -0700

    Still trying to fix the displaying quiz options

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-/*----------------------- I am GOING INSANE WHILE DOING THIS HOMEWORK >:()--------------*/
+/*----------------------- I am GOING INSANE WHILE DOING THIS HOMEWORK >:( )--------------*/
 
 
 var containerQuestionEl = document.getElementById("question-part");
@@ -129,7 +129,7 @@ var renderStartPage = function () {
 }
 
 var displayQuestion = function (index) {
-  questionEl.innerText = index.q
+  questionEl.innerText = index.q;
   for (var i = 0; i < index.choices.length; i++) {
     var answerbutton = document.createElement('button')
     answerbutton.innerText = index.choices[i].choice
@@ -157,8 +157,154 @@ var answerWrong = function () {
   }
 }
 
+var answerCheck = function (event) {
+  var selectedanswer = event.target;
+  if (arrayShuffledQuestions[QuestionIndex].a === selectedanswer.innerText) {
+    answerCorrect()
+    score = score + 7
+  }
+
+  else {
+    answerWrong()
+    score = score - 1;
+    timeleft = timeleft - 3;
+  };
+  QuestionIndex++
+  if (arrayShuffledQuestions.length > QuestionIndex + 1) {
+    setQuestion()
+  }
+  else {
+    gameover = "true";
+    showScore();
+  }
+}
+/* ------------------------ Show Score--------------------------*/
+
+var showScore = function () {
+  containerQuestionEl.classList.add("hide");
+  containerEndEl.classList.remove("hide");
+  containerEndEl.classList.add("show");
+
+  var scoreDisplay = document.createElement("p");
+  scoreDisplay.innerText = ("Your final score is " + score + "!");
+  containerScoreEl.appendChild(scoreDisplay);
+}
+
+var createHighScore = function (event) {
+  event.preventDefault()
+  var initials = document.querySelector("#initials").value;
+  if (!initials) {
+    alert("Enter your intials!");
+    return;
+  }
+
+  var HighScore = {
+    initials: initials,
+    score: score
+  }
+
+  formInitials.reset();
+
+  /* --------------------------------------------- High Score --------------------------*/
 
 
+  //push and sort scores
+  HighScores.push(HighScore);
+  HighScores.sort((a, b) => { return b.score - a.score });
+
+  //clear visibile list to resort
+  while (listHighScoreEl.firstChild) {
+    listHighScoreEl.removeChild(listHighScoreEl.firstChild)
+  }
+  //create elements in order of high scores
+  for (var i = 0; i < HighScores.length; i++) {
+    var highscoreEl = document.createElement("li");
+    highscoreEl.ClassName = "high-score";
+    highscoreEl.innerHTML = HighScores[i].initials + " - " + HighScores[i].score;
+    listHighScoreEl.appendChild(highscoreEl);
+  }
+
+  saveHighScore();
+  displayHighScores();
+
+}
+//save high score
+var saveHighScore = function () {
+  localStorage.setItem("HighScores", JSON.stringify(HighScores))
+
+}
+
+//load values/ called on page load
+var loadHighScore = function () {
+  var LoadedHighScores = localStorage.getItem("HighScores")
+  if (!LoadedHighScores) {
+    return false;
+  }
+
+  LoadedHighScores = JSON.parse(LoadedHighScores);
+  LoadedHighScores.sort((a, b) => { return b.score - a.score })
+
+
+  for (var i = 0; i < LoadedHighScores.length; i++) {
+    var highscoreEl = document.createElement("li");
+    highscoreEl.ClassName = "high-score";
+    highscoreEl.innerText = LoadedHighScores[i].initials + " - " + LoadedHighScores[i].score;
+    listHighScoreEl.appendChild(highscoreEl);
+
+    HighScores.push(LoadedHighScores[i]);
+
+  }
+}
+
+//display high score screen from link or when intiials entered
+var displayHighScores = function () {
+
+  containerHighScoresEl.classList.remove("hide");
+  containerHighScoresEl.classList.add("show");
+  gameover = "true"
+
+  if (containerEndEl.className = "show") {
+    containerEndEl.classList.remove("show");
+    containerEndEl.classList.add("hide");
+  }
+  if (containerStartEl.className = "show") {
+    containerStartEl.classList.remove("show");
+    containerStartEl.classList.add("hide");
+  }
+
+  if (containerQuestionEl.className = "show") {
+    containerQuestionEl.classList.remove("show");
+    containerQuestionEl.classList.add("hide");
+  }
+
+  if (correctEl.className = "show") {
+    correctEl.classList.remove("show");
+    correctEl.classList.add("hide");
+  }
+
+  if (wrongEl.className = "show") {
+    wrongEl.classList.remove("show");
+    wrongEl.classList.add("hide");
+  }
+
+}
+//clears high scores
+var clearScores = function () {
+  HighScores = [];
+
+  while (listHighScoreEl.firstChild) {
+    listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+  }
+
+  localStorage.clear(HighScores);
+
+}
+
+loadHighScore()
 
 /*-------------------- Button Click ----------------------- */
 btnStartEl.addEventListener("click", startGame);
+btnGoBackEl.addEventListener("click", renderStartPage);
+btnClearScoresEl.addEventListener("click", clearScores);
+ViewHighScoreEl.addEventListener("click", displayHighScores);
+formInitials.addEventListener("submit", createHighScore);
